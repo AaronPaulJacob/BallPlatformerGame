5a273f80dd0ebac0edc54d8515ef8dd24ec26ca8
Aaron Paul Jacob <aaron.pauljacob@Aarons-MacBook-Pro.local>
2021-05-01 12:55:00 +0530

    Added Collisions

--- level1.py
+++ level1.py
@@ -49,9 +49,11 @@ N_col= round(screen_width/tile_Size)
 #     r = [0] * N_col
 #     world_data.append(r)
 #Loading level1 world data from the file
-if path.exists(f'level{level}_data'):
-            pickle_in = open(f'level{level}_data', 'rb')
-            world_data = pickle.load(pickle_in)
+# if path.exists(f'level{level}_data'):
+#             pickle_in = open(f'level{level}_data', 'rb')
+#             world_data = pickle.load(pickle_in)
+#             print(world_data)
+world_data = [[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 11, 1, 1, 1, 1, 1, 2], [2, 0, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2], [2, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 9, 9, 0, 0, 0, 0, 0, 0, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 0, 0, 0, 1, 1, 1, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2], [2, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 2], [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2], [2, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 2]]
 def draw_text(text,font,col,x,y):
     img = font.render(text, True, col)
     screen.blit(img, (x,y))
@@ -76,36 +78,42 @@ class World():
             img = tile[0]
             pos = tile[1]
             screen.blit(img,pos)
+            pygame.draw.rect(screen,(255,255,255),pos,2)
 
 class Player():
     def __init__(self, x ,y):
         # creating a series of images for moving animation 
-        self.images_right = []
-        self.images_left = []
-        self.index=0
-        self.counter=0 # speed of player animaion
-        for num in range(1,5):
-            img_right = pygame.image.load(f'img/redBall{num}.png')
-            img_right = pygame.transform.scale(img_right,(tile_Size,tile_Size))
-            img_left = pygame.transform.flip(img_right, True,False) # flip the image across x axis but not across y axis
-            self.images_right.append(img_right)
-            self.images_left.append(img_left)
-        self.image = self.images_right[self.index]
-        self.direction = 0
+        # self.images_right = []
+        # self.images_left = []
+        # self.index=0
+        # self.counter=0 # speed of player animaion
+        # for num in range(1,5):
+        #     img_right = pygame.image.load(f'img/redBall{num}.png')
+        #     img_right = pygame.transform.scale(img_right,(tile_Size,tile_Size))
+        #     img_left = pygame.transform.flip(img_right, True,False) # flip the image across x axis but not across y axis
+        #     self.images_right.append(img_right)
+        #     self.images_left.append(img_left)
+        # self.image = self.images_right[self.index]
+        # self.direction = 0
         #************************ end of animation
               
         img = pygame.image.load('Images/red_ball.png')
         self.image = pygame.transform.scale(img,(tile_Size,tile_Size))
+        print("Image")
+        # print(self.image)
         self.rect= self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        # print(self.rect)
         self.vel_y = 0 # upward velocity on pressing space
         self.jumped = False # Have I jumped or not by pressing the space button
 
     def update(self):
         dx = 0
         dy = 0
-        walk_cooldown = 20
+        # walk_cooldown = 20
         #get keypresses
         key = pygame.key.get_pressed()
         if key[pygame.K_SPACE] and self.jumped == False :  # to allow only 1 jump
@@ -115,29 +123,29 @@ class Player():
             self.jumped = False
         if key[pygame.K_LEFT]:
             dx -= 5
-            self.direction = -1
-            self.counter += 1   # cycle through images only when the left or right keys are pressed 
-            # if counter is incremented and exceedds the walk_cooldown only then the image index will be incremented
+            # self.direction = -1
+            # self.counter += 1   # cycle through images only when the left or right keys are pressed 
+            # # if counter is incremented and exceedds the walk_cooldown only then the image index will be incremented
         if key[pygame.K_RIGHT]:
             dx += 5
-            self.counter += 1
-            self.direction = 1
-        if key[pygame.K_LEFT] == False or key[pygame.K_RIGHT] == False:
-            self.counter=0
-            if self. direction == 1:
-                self.image = self.images_right[self.index]
-            if self.direction == -1:
-                self.image = self.images_left[self.index]
-            self.index=0    # so that animation returns to begining image
+        #     self.counter += 1
+        #     self.direction = 1
+        # if key[pygame.K_LEFT] == False or key[pygame.K_RIGHT] == False:
+        #     self.counter=0
+        #     if self. direction == 1:
+        #         self.image = self.images_right[self.index]
+        #     if self.direction == -1:
+        #         self.image = self.images_left[self.index]
+        #     self.index=0    # so that animation returns to begining image
             
         #handle animation
-        if self.counter > walk_cooldown:
-            self.counter=0
-            self.index =(self.index+1)%len(self.images_right) # cycling through the images
-            if self. direction == 1:
-                self.image = self.images_right[self.index]
-            if self.direction == -1:
-                self.image = self.images_left[self.index]
+        # if self.counter > walk_cooldown:
+        #     self.counter=0
+        #     self.index =(self.index+1)%len(self.images_right) # cycling through the images
+        #     if self. direction == 1:
+        #         self.image = self.images_right[self.index]
+        #     if self.direction == -1:
+        #         self.image = self.images_left[self.index]
         #calculate new player position and check collision at new position 
 
         #adding gravity
@@ -147,6 +155,22 @@ class Player():
         dy += self.vel_y
         
         #check collision
+        for tile in world.tile_list:
+            #check for collision in x direction
+            if tile[1].colliderect(self.rect.x+dx ,self.rect.y ,self.width, self.height):
+                dx = 0
+            #check for collision in y direction
+            if tile[1].colliderect(self.rect.x ,self.rect.y+dy ,self.width, self.height):
+                # if collision is taking place then check
+                
+                # check if below the ground i.e. jumping
+                if self.vel_y < 0:
+                    dy = tile[1].bottom - self.rect.top # resetting the change dy so that it may not go above the block
+                    self.vel_y = 0
+                #check ig above the ground i.e. falling
+                elif self.vel_y >= 0 :
+                    dy = tile[1].top - self.rect.bottom
+                    self.vel_y = 0
 
         #update player coordinates
         self.rect.x += dx
@@ -160,7 +184,7 @@ class Player():
 
         #draw player on screen
         screen.blit(self.image,self.rect)
-
+        pygame.draw.rect(screen,(255,255,255),self.rect,2)
 
 world = World(world_data)
 player = Player(2*tile_Size,screen_height-2*tile_Size)        
